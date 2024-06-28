cd86ef060a70169e3018f1c2e6b62b2d389f8291
Niketan <niketansahu1999@gmail.com>
2024-06-28 19:31:00 +0530

    Added 02counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setcounter] = useState(5)
+  // let counter = 5;
+  const removeValue=()=>{
+    // counter=counter-1;
+    if(counter>0)
+    setcounter(counter-1);
+    console.log("Remove value", counter)
+  }
+  const addValue = ()=>{
+    // counter=counter+1;
+    if(counter<20)
+    setcounter(counter+1);
+    console.log("valueAdded",counter);
+  }
+  return (
+    <>
+      <h1>Niketan Counter {counter}</h1>
+      <button onClick={addValue}>Add Value {counter}</button>
+      <button onClick={removeValue}>Remove Value {counter}</button>
+    </>
+  )
+}
+
+export default App
